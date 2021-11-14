ed370779e4e7383476b362d81066f144c17f2aa5
Samuel <sarodriguez18@gmail.com>
2021-11-14 12:25:58 +0100

    Refactor: Fix console messages

--- src/proyectofuturo/ProyectoFuturo.java
+++ src/proyectofuturo/ProyectoFuturo.java
@@ -14,17 +14,17 @@ public class ProyectoFuturo {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Estudiante " + args[0] + " : " + args[1] + " mÃ³dulos matriculados y " + args[2] + " horas semanales dedicadas al estudio");
+        System.out.println("Estudiante " + args[0] + " : " + args[1] + " modulos matriculados y " + args[2] + " horas semanales dedicadas al estudio");
         
         System.out.println(veredicto(args[1], args[2]));
     }
     
     private static String veredicto(String value1, String value2){
         if (value1.equals(value2)) {
-            return "Bien. Pero una hora de estudio para cada mÃ³dulo puede ser insuficiente.";
+            return "Bien. Pero una hora de estudio para cada modulo puede ser insuficiente.";
         }
         if (Integer.valueOf(value1) > Integer.valueOf(value2)){
-            return "Poco tiempo de estudio. Debes dedicar mÃ¡s tiempo.";
+            return "Poco tiempo de estudio. Debes dedicar mas tiempo.";
         }
         return "Ideal. Trabajas los contenidos en casa.";
     }
